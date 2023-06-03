25168f79912d2d0546e62ebaf3536c0671adfc36
DeVecchis <52166442+DeVecchis@users.noreply.github.com>
2023-06-03 11:53:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from jnius import autoclass, cast
+
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivymd.app import MDApp
 from threading import Thread
 import time
 import socketio
+import pyttsx3
+import sounddevice as sd
+import speech_recognition as sr
 
 sio = socketio.Client()
 
@@ -23,13 +24,6 @@ class AuraApp(MDApp):
 
         self.last_word_time = time.time()
 
-        # Inizializza l'oggetto PythonActivity
-        PythonActivity = autoclass('org.kivy.android.PythonActivity')
-        self.android_activity = PythonActivity.mActivity
-
-        # Sovrascrivi il metodo onActivityResult per gestire la risposta del riconoscimento vocale
-        PythonActivity.mActivity.onActivityResult = self.on_activity_result
-
         # Avvia il riconoscimento vocale in un thread separato
         thread = Thread(target=self.listen_for_speech)
         thread.daemon = True
@@ -38,71 +32,88 @@ class AuraApp(MDApp):
         return kv
 
     def listen_for_speech(self):
-        intent = autoclass('android.content.Intent')()
-        intent.setAction("android.speech.action.RECOGNIZE_SPEECH")
-        intent.putExtra("android.speech.extra.LANGUAGE_MODEL", "free_form")
-        print(intent.__dict__)
-        print("sto per avviare láttività di riconoscimento...")
-        print("attivo in:")
-        for n in range(0,5):
-            print(5-n)
-            time.sleep(1)
-        # Avvia l'activity di riconoscimento vocale
-        r = self.android_activity.startActivityForResult(intent, 0)
-        time.sleep(10)
-        print("------------------------------------------")
-        print("provo a ripetere la cosa ma con 1:")
-        print("attivo in:")
-        for n in range(0,5):
-            print(5-n)
-            time.sleep(1)
-        r = self.android_activity.startActivityForResult(intent, 1)
-        print(r)
-        print(type(r))
-        print(r.__dict__)
+        # Configura il riconoscimento vocale con speech_recognition
+        recognizer = sr.Recognizer()
+
+        # Imposta il dispositivo audio di input al microfono predefinito
+        input_device = sd.default.device[0]
+
+        # Imposta i parametri audio per la registrazione
+        sample_rate = 16000  # Frequenza di campionamento in Hz
+        duration = 4  # Durata della registrazione in secondi
+
+        print("Registrazione in corso...")
+        while True:
+            # Acquisisci l'audio dal microfono utilizzando sounddevice
+            audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, blocking=True, dtype='int16')
+            text=''
+            # Converte l'audio in testo utilizzando speech_recognition
+            try:
+                audio = audio.flatten()
+                audio_data = sr.AudioData(audio.tobytes(), sample_rate, sample_width=2)
+                text = recognizer.recognize_google(audio_data, language="it-IT")  # Modifica la lingua in base alle tue esigenze
+            except sr.UnknownValueError:
+                print("Impossibile convertire l'audio in testo.")
+            except sr.RequestError as e:
+                print("Errore durante la richiesta al servizio di riconoscimento vocale:", str(e))
+            # Dividi la trascrizione in parole
+            words = text.split()
+
+            aura_index = ''
+            # Verifica se la parola "Aura" è stata pronunciata
+            if "Maura" in words:
+                aura_index = words.index("Maura")
+            elif "Aura" in words:
+                aura_index = words.index("Aura")
+            elif "Laura" in words:
+                aura_index = words.index("Laura")
+
+            if aura_index:
+                # Estrai le parole successive alla parola "Aura"
+                aura_words = words[aura_index + 1:]
+
+                # Stampa le parole dopo la parola "Aura" fino a quando non passano più di 3 secondi tra una parola e l'altra
+                for word in aura_words:
+                    print(" ".join(aura_words))
+                    sentence = " ".join(aura_words)
+                    print(sentence)
+
+                    # Invia la variabile al server
+                    sio.emit('sentence', sentence)
+
+                    # Verifica se è passato più di 3 secondi tra una parola e l'altra
+                    current_time = time.time()
+                    if current_time - self.last_word_time > 3:
+                        break
+
+                    self.last_word_time = current_time
+            # # Verifica se è passato più di 3 secondi tra una registrazione e l'altra
+            # current_time = time.time()
+            # if current_time - self.last_word_time > 3:
+            #     break
+
+            self.last_word_time = current_time
+
     def on_stop(self):
         # Ferma l'ascolto quando l'app viene chiusa
-        self.android_activity.onActivityResult = None
-
-    def on_activity_result(self, requestCode, resultCode, data):
-        if requestCode == 0 and resultCode == self.android_activity.RESULT_OK:
-            results = data.getStringArrayListExtra("android.speech.extra.RESULTS")
-
-            if results:
-                transcription = results.get(0)
-                print(transcription)
-
-                words = transcription.split()
-
-                aura_index = ''
-                if "Maura" in words:
-                    aura_index = words.index("Maura")
-                elif "Aura" in words:
-                    aura_index = words.index("Aura")
-                elif "Laura" in words:
-                    aura_index = words.index("Laura")
-
-                if aura_index:
-                    aura_words = words[aura_index + 1:]
-
-                    for word in aura_words:
-                        print(" ".join(aura_words))
-                        sentence = " ".join(aura_words)
-                        print(sentence)
-
-                        sio.emit('sentence', sentence)
-
-                        current_time = time.time()
-                        if current_time - self.last_word_time > 3:
-                            break
-
-                        self.last_word_time = current_time
+        pass
 
     @staticmethod
     @sio.on('response')
     def receive_response(response):
         print("Risposta dal server:", response)
+        # Esegui le operazioni necessarie con la risposta
+        # Riproduci la risposta tramite sintesi vocale
+        engine = pyttsx3.init()
+        
+        # Imposta la voce femminile in italiano
+        engine.setProperty('voice', 'it')
+        
+        engine.setProperty('rate', 150)  # Velocità della voce (default: 200)
+        engine.say(response)
+        engine.runAndWait()
+        engine.stop()
 
 if __name__ == "__main__":
-    sio.connect('http://10.10.10.200:8000')  # Connessione al server Flask
+    sio.connect('http://127.0.0.1:8000')  # Connessione al server Flask
     AuraApp().run()
