526e55ffcdf14601548d368ea79b1eaba8d747bb
DeVecchis <52166442+DeVecchis@users.noreply.github.com>
2023-06-17 22:12:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class AuraApp(MDApp):
             # Converte i dati audio in formato utilizzabile da SpeechRecognition
             audio_data = bytes(audio_buffer)
             sio.emit('sentence', audio_data)
-            time.sleep(2)
+            time.sleep(2.5)
             # Crea un oggetto AudioData utilizzando i dati audio
 
     def on_stop(self):
