9a79f0450c2b5bda486f14d882e2e81b2c375d72
DeVecchis <52166442+DeVecchis@users.noreply.github.com>
2023-06-19 00:00:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -115,5 +115,5 @@ class AuraApp(MDApp):
         print("sono dopo tutto")
 
 if __name__ == "__main__":
-    sio.connect("http://10.10.10.200:8000")  # Connessione al server Flask
+    sio.connect("http://176.12.93.138:8000")  # Connessione al server Flask
     AuraApp().run()
